86eca600997d85a1dab8375b39ad552d8cf84409
Sikandar <sikandar.ramzan@codeninjaconsulting.com>
2022-11-03 22:50:58 +0500

    added fetch users call on mounting phase in additon to onclick

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import UserCard from "./userCard";
 import { InfinitySpin } from "react-loader-spinner";
-// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 var fetchIcon = require("./assets/fetch-icon-sm.png");
-
-// https://randomuser.me/api/
 
 export interface userType {
   userImageURL: string;
@@ -23,6 +20,7 @@ function App() {
   const [users, setUsers] = useState<userType[]>();
   const [isLoading, setisLoading] = useState(false);
 
+  // Fetch users on button click
   const fetchUser = async () => {
     setisLoading(true);
     const response = await fetch("https://randomuser.me/api/?results=100");
@@ -30,6 +28,7 @@ function App() {
 
     const userList = userData.results;
 
+    //Map the response data to what's needed
     const customUserList: userType[] = [];
     userList.forEach((user: any) => {
       customUserList.push({
@@ -46,11 +45,39 @@ function App() {
 
     setUsers([...customUserList]);
   };
-  console.log(users);
 
+  //Hides the loader after the api call is finished i.e users are set in the state
   useEffect(() => {
     setisLoading(false);
   }, [users]);
+
+  // Fetches 10 users on component mount
+  useEffect(() => {
+    setisLoading(true);
+
+    fetch("https://randomuser.me/api/?results=10")
+      .then((res) => res.json())
+      .then((result) => {
+        const userList = result.results;
+
+        const customUserList: userType[] = [];
+
+        userList.forEach((user: any) => {
+          customUserList.push({
+            userImageURL: user.picture.large,
+            name: `${user.name.first} ${user.name.last}`,
+            gender: user.gender,
+            email: user.email,
+            city: user.location.city,
+            state: user.location.state,
+            country: user.location.country,
+            contact: user.cell,
+          });
+        });
+
+        setUsers([...customUserList]);
+      });
+  }, []);
 
   return (
     <div>
@@ -66,7 +93,9 @@ function App() {
       </div>
 
       {isLoading ? (
-        <InfinitySpin width="200" color="#8a15fff7" />
+        <div className="loader-animation">
+          <InfinitySpin width="200" color="#8a15fff7" />
+        </div>
       ) : (
         users?.map((user, idx) => {
           return (
